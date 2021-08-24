4445ba05afe5114b5a324b95df105c622979fa99
Aryan Chauhan <aryanchauhan5003@gmail.com>
2021-08-24 11:56:41 +0530

    handled variable length binary sequences

--- rnn_to_handle_variable_length_input.py
+++ rnn_to_handle_variable_length_input.py
@@ -3,16 +3,18 @@ import torch.nn as nn
 from torch.utils import data
 import torchvision
 import torchvision.transforms as transforms
-
+from random import randint
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyper-parameters 
-input_size = 32
+maxSeqLen = 256
+input_size= 1
 hidden_size = 4
 num_classes = 2
-num_epochs = 20
+num_epochs = 1
+num_layers=1
 batch_size = 100
 learning_rate = 0.001
 numberOfBatches=10000
@@ -20,45 +22,52 @@ numberOfBatches=10000
 train_data=[]
 test_data=[]
 
+
 for x in range(0,numberOfBatches):
-    from random import randint
     images=[]
     labels=[]
-    for i in range(0,100):
+    seqLength=randint(1,maxSeqLen)
+    for i in range(0,batch_size):
         temp=[]
-        for j in range(0,input_size):
-            temp.append(float(randint(0,1)))
+        for j in range(0,seqLength):
+            temp.append([float(randint(0,1))])
         images.append(temp)
-        labels.append(int(temp[31])%2)
+        labels.append(int(temp[-1][0]))
     train_data.append((torch.tensor(images),torch.tensor(labels)))
 
-for x in range(0,600):
-    from random import randint
+for x in range(0,int(numberOfBatches/10)):
     images=[]
     labels=[]
-    for i in range(0,100):
+    seqLength=randint(1,maxSeqLen)
+    for i in range(0,batch_size):
         temp=[]
-        for j in range(0,input_size):
-            temp.append(float(randint(0,2)))
+        for j in range(0,seqLength):
+            temp.append([float(randint(0,1))])
         images.append(temp)
-        labels.append(int(temp[31])%2)
+        labels.append(int(temp[-1][0]))
     test_data.append((torch.tensor(images),torch.tensor(labels)))
 
-# Fully connected neural network with one hidden layer
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size) 
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)  
-    
+# Recurrent neural network (many-to-one)
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True,nonlinearity='relu')
+        self.fc = nn.Linear(hidden_size , num_classes)
+
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
+        # Set initial hidden and cell states
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+
+        # Forward propagate LSTM
+        out, _ = self.rnn(x, h0)
+
+        # Decode the hidden state of the last time step
+        out = self.fc(out[:,-1,:])
         return out
 
-model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+model = RNN(input_size=input_size, hidden_size=hidden_size,num_layers=num_layers ,num_classes=num_classes).to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -69,13 +78,13 @@ total_step = len(train_data)
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_data):  
         # Move tensors to the configured device
-        images = images.reshape(-1,input_size).to(device)
+        images = images.to(device)
         labels = labels.to(device)
         
         # Forward pass
         outputs = model(images)
-        # print(labels.size())
         # print(outputs.size())
+        # print(labels.size())
         loss = criterion(outputs, labels)
         
         # Backward and optimize
@@ -96,7 +105,7 @@ with torch.no_grad():
     correct = 0
     total = 0
     for images, labels in test_data:
-        images = images.reshape(-1,input_size).to(device)
+        images = images.to(device)
         labels = labels.to(device)
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
@@ -109,3 +118,5 @@ with torch.no_grad():
         #     break
 
     print('Accuracy of the network on the 6000 test images: {} %'.format(100 * correct / total))
+
+
